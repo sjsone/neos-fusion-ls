1b3165217f7c82767a1b71526bee398a8beac6d8
sjsone <simon@sjs.one>
2025-11-21 23:16:04 +0100

    fix: resourceUri completion

--- src/elements/ResourceUriElement.ts
+++ src/elements/ResourceUriElement.ts
@@ -1,11 +1,16 @@
 import * as NodeFs from 'fs';
 import * as NodePath from 'path';
-import { Hover, HoverParams } from 'vscode-languageserver';
+import { Command, CompletionItem, CompletionItemKind, Hover, HoverParams } from 'vscode-languageserver';
 import { ResourceUriNode } from '../fusion/node/ResourceUriNode';
 import { CapabilityContext } from './CapabilityContext';
 import { Element } from './Element';
+import { NeosPackage } from '../neos/NeosPackage';
 
 export class ResourceUriElement extends Element<ResourceUriNode> {
+	static SuggestCommand: Command = {
+		title: 'Trigger Suggest',
+		command: 'editor.action.triggerSuggest'
+	}
 	public async hoverCapability(context: CapabilityContext<ResourceUriNode>, params: HoverParams): Promise<string | Hover | undefined> {
 		const node = context.foundNodeByLine!.getNode()
 		if (!(node instanceof ResourceUriNode)) return undefined
@@ -21,5 +26,53 @@ export class ResourceUriElement extends Element<ResourceUriNode> {
 
 		if (isImage) return `![${basename}](${path})`
 		return `Resource: ${basename}`
+	}
+
+	public async completionCapability(context: CapabilityContext<ResourceUriNode>): Promise<CompletionItem[] | undefined> {
+		const foundNode = context.foundNodeByLine
+		if (foundNode === undefined) return undefined
+
+		const node = foundNode.getNode()
+		if (!(node instanceof ResourceUriNode)) return undefined
+
+		const workspace = context.workspaces[0]!
+
+		const identifierMatch = /resource:\/\/(.*?)\//.exec(node.identifier)
+		if (identifierMatch === null) {
+			return Array.from(workspace.neosWorkspace.getPackages().values()).map((neosPackage: NeosPackage) => {
+				return {
+					label: neosPackage.getPackageName(),
+					kind: CompletionItemKind.Module,
+					insertText: neosPackage.getPackageName() + '/',
+					command: ResourceUriElement.SuggestCommand
+				}
+			})
+		}
+		const packageName = identifierMatch[1]
+
+		const neosPackage = workspace.neosWorkspace.getPackage(packageName)
+		if (!neosPackage) return []
+
+		const nextPath = NodePath.join(neosPackage.path, "Resources", node.getRelativePath())
+		if (!NodeFs.existsSync(nextPath)) return []
+
+		const completions: CompletionItem[] = []
+		const thingsInFolder = NodeFs.readdirSync(nextPath, { withFileTypes: true })
+		for (const thing of thingsInFolder) {
+			if (thing.isFile()) completions.push({
+				label: thing.name,
+				kind: CompletionItemKind.File,
+				insertText: thing.name,
+			})
+
+			if (thing.isDirectory()) completions.push({
+				label: thing.name,
+				kind: CompletionItemKind.Folder,
+				insertText: thing.name + '/',
+				command: ResourceUriElement.SuggestCommand
+			})
+		}
+
+		return completions
 	}
 }
